85ea4068615b24426afd2cf0827ef99ddb90a79e
ArunMurugan78 <arunmurugan200@gmail.com>
2020-05-20 15:51:28 +0530

    completed bfs

--- src/App.js
+++ src/App.js
@@ -4,25 +4,107 @@ import Grid from "./grid";
 
 export class App extends Component {
   state = {
-    walls:[],
+  
     height:20,
     width:30,
     start:[10,1],
-    end:[10,29]
+    end:[10,29],
+    visited :[],
+    path:[],
+    current:null,
+    grid:Array(20).fill().map(() => Array(30).fill(0))
+  }
+  visualize =  async () => {
+    let path = Array(20).fill().map(() => Array(30).fill([]));
+    let q = [[10,1]]
+    let flag = 1;
+while(q.length!==0 && flag){
+  await new Promise(done => setTimeout(() => done(), 1));  
+  let current = q[0];
+  q.shift()
+  this.setState({current});
+  let grid = this.state.grid;
+
+  if(current[0]===this.state.end[0] && current[1]===this.state.end[1]){
+    
+    console.log(current)
+    flag=0;
+   
+  }
+  else{
+ 
+  let list = [];
+
+  if(current[0]!==this.state.height-1&&grid[current[0]+1][current[1]]===0){
+    list.push([current[0]+1,current[1]]);
+    path[current[0]+1][current[1]] = [...path[current[0]][current[1]],current];
+
+ path[current[0]+1][current[1]].push(current);
+  }
+  if(current[0]!==0 && grid[current[0]-1][current[1]]===0){
+    list.push([current[0]-1,current[1]]);
+    path[current[0]-1][current[1]] = [...path[current[0]][current[1]],current];
+
+  }
+  if(current[1]!==0 && grid[current[0]][current[1]-1]===0){
+    list.push([current[0],current[1]-1]);
+    path[current[0]][current[1]-1] = [ ...path[current[0]][current[1]],current];
+
+  }
+  if(current[1]!==this.state.width-1 && grid[current[0]][current[1]+1]===0){
+    list.push([current[0],current[1]+1]);
+    path[current[0]][current[1]+1] = [...path[current[0]][current[1]],current];
+
+   
+  }
+  console.log("list",list);
+
+  for(let i=0;i<list.length;i++){
+    grid[list[i][0]][list[i][1]] = 2;
+  }
+  grid[current[0]][current[1]] = 3;
+
+  this.setState({grid});
+    q = q.concat(list);}
+  
+}
+//console.log(path[this.state.end[0]][this.state.end[1]].length)
+this.setState({path:path[this.state.end[0]][this.state.end[1]]})
   }
   toggleWall = (y,x,type) => {
-    console.log("hello")
+    
     if(type===1){
-      this.setState(state => ({walls : state.walls.filter(obj => !(obj[0]===y && obj[1]===x))}))
+     
+      this.setState(state => {
+        let grid = state.grid;
+        grid[y][x] = 0;
+        return {grid}
+      })
     }
     else{
-      this.setState(state => ({walls:[...state.walls,[y,x]]}));
+     
+      this.setState(state => {
+        let grid = state.grid;
+        grid[y][x] = 1;
+        return {grid}
+      })
     }
   }
   render() {
     return (
     <div>
-      <Grid height={this.state.height} start={this.state.start} end={this.state.end} width={this.state.width} walls={this.state.walls} toggleWall={this.toggleWall}/>
+      <div className="navbar navbar-light">
+ <div className="navbar-brand">
+   Shortest Path Finder
+ </div>
+ <button className="nav-item btn" onClick={()=>this.visualize()}>
+find The path
+ </button>
+ <button className="nav-item btn" onClick={() => this.setState({grid:Array(20).fill().map(() => Array(40).fill(0)),path:[]})}>
+ Clear Walls
+ </button>
+      </div>
+      <Grid height={this.state.height} grid={this.state.grid} path={this.state.path} current={this.state.current}  start={this.state.start} end={this.state.end} width={this.state.width}  toggleWall={this.toggleWall}/>
     </div>);
   }
 }
--- src/grid.js
+++ src/grid.js
@@ -1,35 +1,53 @@
 import React from "react";
 import start from './play-solid.svg';
+import './grid.css'
 import end from './times-circle-solid.svg'
-const is_wall = (walls,y,x) => {
+const is_present = (arr,y,x) => {
   //console.log(walls)
-  return walls.reduce((t,c)=>{
+  return arr.reduce((t,c)=>{
     if(c[0]===y && c[1]=== x){
       return true
     }
     return t;
   },false)
 }
+
 function Grid(props) {
   
   const height = props.height ||20 ;
   const width = props.width || 30;
   let list = [];
-  const walls = props.walls;
+
   for(let i =0;i<height;i++){
     let temp = []
       for(let j=0;j<width;j++){
-        if(i===props.start[0] && j===props.start[1]){
-          temp.push(<div  key={i+j} style={{width:'30px',height:'30px',border:'1px solid lightblue',webkitUserSelect: 'none'}}><img src={start} alt="start" style={{width:'25px',height:'25px'}}/></div>)
+         if(i===props.end[0] && j===props.end[1]){
+          temp.push(<div  key={i+j} style={{width:'30px',height:'30px',border:'1px solid lightblue',WebkitUserSelect: 'none'}}><img src={end} alt="start" style={{width:'25px',height:'25px'}}/></div>)
         }
-        else if(i===props.end[0] && j===props.end[1]){
-          temp.push(<div  key={i+j} style={{width:'30px',height:'30px',border:'1px solid lightblue',webkitUserSelect: 'none'}}><img src={end} alt="start" style={{width:'25px',height:'25px'}}/></div>)
+        else if(i===props.start[0] && j===props.start[1]){
+          temp.push(<div  key={i+j} style={{width:'30px',height:'30px',border:'1px solid lightblue',WebkitUserSelect: 'none'}}><img src={start} alt="start" style={{width:'25px',height:'25px'}}/></div>)
         }
-        else if(is_wall(walls,i,j)){
-          temp.push(<div key={i+j} className="animate_animated animate__heartBeat" style={{width:'30px',height:'30px',backgroundColor:'#202060',webkitUserSelect: 'none'}}    onMouseOver={(e)=> {if(window.event.buttons===1){props.toggleWall(i,j,1)};}} onClick={(e)=> {props.toggleWall(i,j,1);}} ></div>)
+        else if(is_present(props.path,i,j)){
+          temp.push(<div  key={i+j} className="animation-target " style={{width:'30px',height:'30px',WebkitUserSelect: 'none',backgroundColor:'#fcf876'}}></div>)
+        
+        }
+      
+       else if(props.current && props.current[0]===i&&props.current[1]===j){
+          temp.push(<div  key={i+j} style={{width:'30px',height:'30px',backgroundColor:'#fcf876',WebkitUserSelect: 'none'}}></div>)
+        }
+        else if(props.grid[i][j]===3){
+          temp.push(<div  key={i+j} className="animation-target " style={{width:'30px',height:'30px',backgroundColor:'#3797a4',WebkitUserSelect: 'none'}}></div>)
+        }
+        else if(props.grid[i][j]===2){
+          temp.push(<div  key={i+j} className="animation-target " style={{width:'30px',height:'30px',backgroundColor:'#8ccbbe',WebkitUserSelect: 'none'}}></div>)
+        
+        }
+        
+        else if(props.grid[i][j]===1){
+          temp.push(<div key={i+j} className="animation-target animate__shakeX" style={{width:'30px',height:'30px',backgroundColor:'#202060',WebkitUserSelect: 'none'}} onTouchStart={(e)=> {if(window.event.buttons===1){props.toggleWall(i,j,1)};}}   onMouseOver={(e)=> {if(window.event.buttons===1){props.toggleWall(i,j,1)};}} onClick={(e)=> {props.toggleWall(i,j,1);}} ></div>)
         }
         else{
-          temp.push(<div  key={i+j}  style={{width:'30px',height:'30px',border:'1px solid lightblue',webkitUserSelect: 'none'}}   onMouseOver={(e)=> {if(window.event.buttons===1){props.toggleWall(i,j,0)}}} onClick={(e)=> {props.toggleWall(i,j,1);}}></div>)
+          temp.push(<div  key={i+j}  style={{width:'30px',height:'30px',border:'1px solid lightblue',WebkitUserSelect: 'none'}} onTouchStart={(e)=> {if(window.event.buttons===1){props.toggleWall(i,j,0)}}}  onMouseOver={(e)=> {if(window.event.buttons===1){props.toggleWall(i,j,0)}}} onClick={(e)=> {props.toggleWall(i,j,0);}}></div>)
         }
   
       }
@@ -37,13 +55,11 @@ function Grid(props) {
   list.push(temp)
   }
 
- // console.log(list)
   return (
   <div className="p-4">
  
    {list.map((obj,ind) => {
-//console.log(obj)
-   return <div className="row justify-content-center" key={ind}>{obj}</div>
+   return <div className="row justify-content-center flex-nowrap" key={ind}>{obj}</div>
    })}
   </div>
   );
